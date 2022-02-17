021e06c506c66e804cf9c76ccd93e1df918375a6
zackramjan <zack.hr@gmail.com>
2022-02-17 01:47:07 +0000

    dropped miliwatt detail in favor of watts

--- restartHeater.py
+++ restartHeater.py
@@ -13,20 +13,20 @@ def logIt(msg):
     print(str(datetime.now()) + ": " + msg, file=sys.stderr, flush=True)
 
 def main(argv=None): 
-    heaterActivePower = 60000
+    heaterActivePower = 60
     powerStackSize = 30
     if "POWERDEPTH" in environ:
        powerStackSize = int(environ["POWERDEPTH"])
 
     powerStackMW = []
     for i in range(powerStackSize):
-        powerStackMW.append(1400)
+        powerStackMW.append(1)
 
     while True:
         try:
             plug = SmartPlug("192.168.1.83")
             e = plug.get_emeter_realtime()
-            powerStackMW.append(e['power_mw'])
+            powerStackMW.append(round(e['power_mw']/1000))
             total = sum(powerStackMW)
             logIt("Current state: %s" % plug.state + " - Current consumption: %s" % e + " last: %s" % powerStackMW)
             powerStackMW.pop(0)
@@ -38,12 +38,12 @@ def main(argv=None):
                 plug.turn_on()
                 powerStackMW.append(heaterActivePower)
                 powerStackMW.pop(0)
-        
+       
         except:
             traceback.print_exc()
             powerStackMW = []
             for i in range(powerStackSize - 1):
-                powerStackMW.append(1400)
+                powerStackMW.append(1)
             powerStackMW.append(heaterActivePower)    
 
         time.sleep(60) 
